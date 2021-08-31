c8dc71f84983c4f669a7325cd3a428fc9110ab5e
Vijay Tripathi <vijat@amazon.com>
2021-08-31 15:50:10 +0000

    Upgrade to ACK runtime v0.13.0. (#8)
    Description of changes:
    Upgrade ACK runtime to v0.13.0 and regenerate controller using code-generator v0.13.0
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- pkg/resource/api_mapping/resource.go
+++ pkg/resource/api_mapping/resource.go
@@ -97,10 +97,16 @@ func (r *resource) SetIdentifiers(identifier *ackv1alpha1.AWSIdentifiers) error 
 	}
 	r.ko.Status.APIMappingID = &identifier.NameOrID
 
-	f0, f0ok := identifier.AdditionalKeys["domainName"]
-	if f0ok {
-		r.ko.Spec.DomainName = &f0
+	f1, f1ok := identifier.AdditionalKeys["domainName"]
+	if f1ok {
+		r.ko.Spec.DomainName = &f1
 	}
 
 	return nil
+}
+
+// DeepCopy will return a copy of the resource
+func (r *resource) DeepCopy() acktypes.AWSResource {
+	koCopy := r.ko.DeepCopy()
+	return &resource{koCopy}
 }
