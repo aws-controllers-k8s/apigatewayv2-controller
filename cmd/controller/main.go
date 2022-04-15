aadf04079b589a3174fc972e08bc206e67e2fd66
ci-robot <ci-robot@k8s.io>
2022-04-15 17:42:33 +0000

    Update to ACK runtime `v0.18.4`, code-generator `v0.18.4`

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -18,6 +18,7 @@ package main
 import (
 	"os"
 
+	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
 	ackcfg "github.com/aws-controllers-k8s/runtime/pkg/config"
 	ackrt "github.com/aws-controllers-k8s/runtime/pkg/runtime"
 	ackrtutil "github.com/aws-controllers-k8s/runtime/pkg/util"
@@ -31,7 +32,6 @@ import (
 
 	svctypes "github.com/aws-controllers-k8s/apigatewayv2-controller/apis/v1alpha1"
 	svcresource "github.com/aws-controllers-k8s/apigatewayv2-controller/pkg/resource"
-	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
 
 	_ "github.com/aws-controllers-k8s/apigatewayv2-controller/pkg/resource/api"
 	_ "github.com/aws-controllers-k8s/apigatewayv2-controller/pkg/resource/authorizer"
@@ -40,6 +40,8 @@ import (
 	_ "github.com/aws-controllers-k8s/apigatewayv2-controller/pkg/resource/route"
 	_ "github.com/aws-controllers-k8s/apigatewayv2-controller/pkg/resource/stage"
 	_ "github.com/aws-controllers-k8s/apigatewayv2-controller/pkg/resource/vpc_link"
+
+	"github.com/aws-controllers-k8s/apigatewayv2-controller/pkg/version"
 )
 
 var (
@@ -105,7 +107,11 @@ func main() {
 	)
 	sc := ackrt.NewServiceController(
 		awsServiceAlias, awsServiceAPIGroup, awsServiceEndpointsID,
-		ackrt.VersionInfo{}, // TODO: populate version info
+		ackrt.VersionInfo{
+			version.GitCommit,
+			version.GitVersion,
+			version.BuildDate,
+		},
 	).WithLogger(
 		ctrlrt.Log,
 	).WithResourceManagerFactories(
